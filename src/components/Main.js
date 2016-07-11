c39099a40c37209ed397a79105048eefd0d698c6
Josh Kelley <joshkel@gmail.com>
2016-07-10 22:31:41 -0400

    Finish demo

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,10 +2,17 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
+import {
+  Gateway,
+  GatewayDest,
+  GatewayProvider
+} from 'react-gateway';
+import ReactModal2 from 'react-modal2';
 
 class AppComponent extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {showModal: false};
     this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
@@ -18,10 +25,26 @@ class AppComponent extends React.Component {
 
   render() {
     return (
-      <div className="index" onKeyDown={this.handleKeyDown}>
-        <p>Click on the input element, then press Escape.</p>
-        <input type="text" autoFocus/>
-      </div>
+      <GatewayProvider>
+        <div class="app" onKeyDown={this.handleKeyDown}>
+          <div class="app-content">
+            <h1>My Application</h1>
+            <p>Click on the input element, then press Escape.</p>
+            <input type="text" autoFocus/>
+            <button onClick={() => this.setState({showModal: true})}>Show modal</button>
+            <Gateway into="modal">
+              {this.state.showModal &&
+                <ReactModal2 backdropClassName="modal-backdrop" modalClassName="modal" onClose={() => this.setState({showModal: false})}>
+                  <p>Modal demo</p>
+
+                  <button onClick={() => this.setState({showModal: false})}>Close</button>
+                </ReactModal2>
+              }
+            </Gateway>
+          </div>
+          <GatewayDest name="modal" className="modal-container"/>
+        </div>
+      </GatewayProvider>
     );
   }
 }
